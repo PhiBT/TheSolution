90ca19f12e2a41078571b7a0150712d89beaefff
phibt <phibtp@gmail.com>
2022-03-18 17:10:12 +0700

    A

---
+++ TheSolution.Api/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace TheSolution.Api.Extensions
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static IEndpointRouteBuilder MapTheSolutionApiEndpoints(this IEndpointRouteBuilder endpoints) => endpoints
+
+    }
+}
